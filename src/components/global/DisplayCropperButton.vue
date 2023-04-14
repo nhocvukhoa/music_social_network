7f0fa7229922fd04fa963c29ef04b79d91d37ce7
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-04-14 15:55:32 +0700

    install vue cropper image

--- src/components/global/DisplayCropperButton.vue
+++ src/components/global/DisplayCropperButton.vue
@@ -1,6 +1,7 @@
 <template>
   <label class="block uppercase tracking-wide text-gray-900 text-xs font-bold">{{ label }}</label>
-	<button class="mt-2 inline-block bg-green-500 hover:bg-green-700 font-bold text-white no-underline px-4 py-3"
+	<button 
+		class="mt-2 inline-block bg-green-500 hover:bg-green-700 font-bold text-white no-underline px-4 py-3"
 		@click="$emit('showModal', true)">{{ btnText }}</button>
 </template>
 
